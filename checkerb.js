11d559b28d1b990af24c8d5a9206ed19b995437d
avilano <vira0092@gmail.com>
2016-11-13 14:15:30 -0600

    created test for red vs black tiles, redundacy checks, improved output for user

--- checkerb.js
+++ checkerb.js
@@ -10,7 +10,7 @@ function randPattern(RandBoard) {
 }
 
 function isConsecutive(a,b,c,d) {
-  return ( (a !== 0)&&(a==b)&&(a==c)&&(a==d)&&(b==c)&&(c==d) );
+  return ( (a !== 0)&&(a===b)&&(a===c)&&(a===d)&&(b===c)&&(c===d) );
 }
 
 function mainCheck(tile) { // note that j is X-coordinate and i is Y-coordinate
@@ -21,32 +21,40 @@ function mainCheck(tile) { // note that j is X-coordinate and i is Y-coordinate
   //checks vertically
   for (var i = 0; i < 5; i++) {
     for (var j = 0; j < tile.length; j++) {
-      if ( isConsecutive(tile[i][j], tile[i+1][j], tile[i+2][j], tile[i+3][j]) ) {
-          matchedArr.push(["down",j+1,i+1]);
+      if ( isConsecutive(tile[i][j], tile[i+1][j], tile[i+2][j], tile[i+3][j]) && tile[i][j]===1 ) {
+          matchedArr.push(["Black | Vertically Down from:",j+1,i+1]);
+      } else if ( isConsecutive(tile[i][j], tile[i+1][j], tile[i+2][j], tile[i+3][j]) && tile[i][j]===2 ) {
+          matchedArr.push(["Red | Vertically Down from:",j+1,i+1]);
       }
     }
   }
   //checks horizontally
   for (var i = 0; i  < tile.length; i++) {
     for (var j = 0; j < 5; j++) {
-      if ( isConsecutive(tile[i][j], tile[i][j+1], tile[i][j+2], tile[i][j+3] )) {
-          matchedArr.push(["right",j+1,i+1]);
+      if ( isConsecutive(tile[i][j], tile[i][j+1], tile[i][j+2], tile[i][j+3]) && tile[i][j]===1 ) {
+          matchedArr.push(["Black | Horizontally right from:",j+1,i+1]);
+      } else if ( isConsecutive(tile[i][j], tile[i][j+1], tile[i][j+2], tile[i][j+3]) && tile[i][j]===2 ) {
+          matchedArr.push(["Red | Horizontally right from:",j+1,i+1]);
       }
     }
   }
   //checks diagonally-right
   for (var i = 0; i  < 5; i++) {
     for (var j = 0; j < 5; j++) {
-      if ( isConsecutive(tile[i][j], tile[i+1][j+1], tile[i+2][j+2], tile[i+3][j+3] )) {
-          matchedArr.push(["diagright",j+1,i+1]);
+      if ( isConsecutive(tile[i][j], tile[i+1][j+1], tile[i+2][j+2], tile[i+3][j+3]) && tile[i][j]===1 ) {
+          matchedArr.push(["Black | Diagonally Down from:",j+1,i+1]);
+      } else if ( isConsecutive(tile[i][j], tile[i+1][j+1], tile[i+2][j+2], tile[i+3][j+3]) && tile[i][j]===2 ) {
+          matchedArr.push(["Red | Diagonally Down from:",j+1,i+1]);
       }
     }
   }
   //checks diagonally-left
   for (var i = 3; i  < 8; i++) {
     for (var j = 0; j < 5; j++) {
-      if ( isConsecutive(tile[i][j], tile[i-1][j+1], tile[i-2][j+2], tile[i-3][j+3] )) {
-          matchedArr.push(["diagleft",j+1,i+1]);
+      if ( isConsecutive(tile[i][j], tile[i-1][j+1], tile[i-2][j+2], tile[i-3][j+3]) && tile[i][j]===1 ) {
+          matchedArr.push(["Black | Diagonally Up from:",j+1,i+1]);
+      } else if ( isConsecutive(tile[i][j], tile[i-1][j+1], tile[i-2][j+2], tile[i-3][j+3]) && tile[i][j]===1 ) {
+          matchedArr.push(["Red | Diagonally Down from:",j+1,i+1]);
       }
     }
   }
@@ -66,10 +74,12 @@ var checkerArr = [
   [0,0,0,0,0,0,0,0]
 
 ];
-var randomizedArr = randPattern(checkerArr);
+var randomizedArr = randPattern(checkerArr)
+var resultArr = mainCheck(randomizedArr)
 
 console.log("This is a randomized 8X8 Checker board [0 are blanks, 1 are blacks, 2 are reds] :");
 console.log(randomizedArr);
 console.log("-------------------------------");
-console.log("Here are the places where you have 4 similar tiles in a row:");
-console.log(mainCheck(randomizedArr));
+console.log("There was a total of " + resultArr.length + " places found");
+console.log("here is where you have four similar tiles in a row:");
+console.log(resultArr);
