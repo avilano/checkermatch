d3f967c8d6ad8c20af2eb2a99ef9eda77698d970
avilano <vira0092@gmail.com>
2016-11-14 16:44:56 -0600

    re-learning perl

--- checkerb.pl
+++ checkerb.pl
@@ -1,1 +1,32 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
+
+#Randomizes the 2D Array
+sub randPattern {
+my ($rand_ref) = @_;
+my @RandBoard = @{ $rand_ref };
+
+for (my $i = 0; $i <= $#RandBoard; $i++) {
+  for (my $j = 0; $j <= $#RandBoard ; $j++) {
+     $RandBoard[$i][$j] = 0 + int(rand(3 - 0));
+     #print "$RandBoard[$i][$j] ";
+   }
+   #print "\n";
+  }
+  return @RandBoard;
+}
+
+my @checkerArr =  (
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0]
+);
+
+my @randomizedArr = randPattern(\@checkerArr);
+print @randomizedArr, "\n";
